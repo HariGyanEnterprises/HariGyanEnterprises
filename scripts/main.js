1cf2765182aa488fc051b5287af4f4f47528ffd6
HariGyanEnterprises <harigyanenterprise@gmail.com>
2024-09-18 23:36:00 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
+let currentLanguage = 'en';
+
+function toggleLanguage() {
+  const elements = document.querySelectorAll('[data-en]');
+  currentLanguage = currentLanguage === 'en' ? 'hi' : 'en';
+
+  elements.forEach((el) => {
+    el.textContent = el.getAttribute(`data-${currentLanguage}`);
+  });
+
+  // Update the button text
+  const langToggleButton = document.getElementById('language-toggle');
+  langToggleButton.textContent = currentLanguage === 'en' ? 'हिंदी' : 'English';
+}
 
